f4aa27cf542ac2de82ac90bdfc8fb86d6c0e9b89
Victor De La Cruz <victor_de_la_cruz@claro.com.do>
2018-12-20 16:08:35 -0400

    first commit

---
+++ KatalonAPITesting/JsonServer/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var request = require("request");
+
+app.get('/get', function (req, res) {
+    
+ res.send('Hello World');
+})
+
+var server = app.listen(5000, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
---
+++ KatalonAPITesting/KatalonAPITesting-master/README.md
@@ -0,0 +1,7 @@
+# Katalon API Testing
+Repo contains Katalon API Testing related code like GET/POST/DELETE/PUT
+
+# YouTube video link
+GET Request - https://www.youtube.com/watch?v=zNhLkdKcwE8
+
+POST Request - https://www.youtube.com/watch?v=v-eBvfkLg-8
